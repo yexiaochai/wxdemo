a0ac6ed0653e9b177ae37d5db85a2034871a9325
yexiaochai <549265480@qq.com>
2018-08-01 15:54:16 +0800

    改造结束

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,22 +3,17 @@ const _page = require('../../utils/abstract-page.js');
 //获取应用实例
 const app = getApp()
 
-
 Page(_page.initPage({
   data: {
-    ttt: 'ttt'
-
   },
   // methods: uiUtil.getPageMethods(),
   methods: {
   },
   onShow: function () {
+     global.sss = this;
      let scope = this;
      this.showToast('我是美丽可爱的toast');
-     // 3秒后关闭loading
-    //  setTimeout(function () {
-    //    scope.hideToast();
-    //  }, 3000);
+     
   },
   onLoad: function () {
     // this.setPageMethods();
